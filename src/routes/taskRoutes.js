1dbc6ea7022e0a5c8aa2f83ba7a5e65b9a152bd0
Sarjak Bhandari <bhandarisarjak@gmail.com>
2026-06-27 01:08:39 +0545

    All pending routes impl

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,6 +1,15 @@
 import { Router } from 'express';
 
-import { createTask, deleteTask, listTasks, snoozeTask, updateTask } from '../controllers/taskController.js';
+import {
+  createTask,
+  deleteTask,
+  disableExamMode,
+  enableExamMode,
+  listTasks,
+  snoozeTask,
+  sortTasksByPriority,
+  updateTask
+} from '../controllers/taskController.js';
 import { requireAuth } from '../middleware/auth.js';
 
 const taskRoutes = Router();
@@ -9,6 +18,9 @@ taskRoutes.use(requireAuth);
 
 taskRoutes.get('/', listTasks);
 taskRoutes.post('/', createTask);
+taskRoutes.post('/sort-by-priority', sortTasksByPriority);
+taskRoutes.post('/exam-mode/enable', enableExamMode);
+taskRoutes.post('/exam-mode/disable', disableExamMode);
 taskRoutes.patch('/:id', updateTask);
 taskRoutes.post('/:id/snooze', snoozeTask);
 taskRoutes.delete('/:id', deleteTask);
